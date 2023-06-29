2310cb6237704b4ce5c5aa2955f06d1411623598
jdramirezs <42697791+jdramirezs@users.noreply.github.com>
2023-06-28 20:49:01 -0500

    version2

---
+++ Assets/Scripts/Scripts_old/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // hola compañero estos es la funcion start
+    void Start()
+    {
+        Debug.Log("aca es el start");
+        gameObject.transform.position = new Vector3(0, 3.62f, 0);
+    }
+
+    // esto es la funcion update
+    void Update()
+    {
+        Debug.Log("aca es el update");
+        //gameObject.transform.position = new Vector3(gameObject.transform.position.x + 2f * Time.deltaTime, 3.62f, 0);               
+
+        if (Input.GetKey("right") && (gameObject.transform.position.x < 11.82))
+        {
+            gameObject.transform.Translate(3.5f * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("left") && (gameObject.transform.position.x >= -11.82))
+        {
+            gameObject.transform.Translate(-3.5f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up") && gameObject.transform.position.y < 4)
+        {
+            gameObject.transform.Translate(0, 3.5f * Time.deltaTime, 0);
+        }
+    }
+}
